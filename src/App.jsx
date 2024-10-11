8172773f525bc00dcc83576ced7351ec3e67ad8b
Lucas Barrera Menem <barreramenemlucas@gmail.com>
2024-10-11 19:56:46 -0300

    primer commit, inicio de repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Card from "./components/card/Card";
+
+function App() {
+  return (
+    <>
+      <Card title="Productos en DB" content="25" />
+    </>
+  );
+}
+
+export default App;
