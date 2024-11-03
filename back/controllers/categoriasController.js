8f048b1ee6b1b98c16d887967ec25509d5a64bfd
cgastonq <cgastonquintero@gmail.com>
2024-11-03 20:24:19 -0300

    correccion para mostrar categorias

--- back/controllers/categoriasController.js
+++ back/controllers/categoriasController.js
@@ -10,48 +10,24 @@ const categoriasController = {
         limit: limit,
         offset: (page - 1) * limit,
         attributes: [
-          [Sequelize.fn("DISTINCT", Sequelize.col("categoria")), "categoria"],
+          "id", "categoria",
         ],
+        group: ["categoria"],
+        order: [["id", "ASC"]],
       });
 
-      const totalPages = Math.ceil(count / limit);
+      const totalPages = Math.ceil(count.length / limit);
 
       res.json({
         categorias,
         currentPage: page,
         totalPages,
-        totalCategories: count,
+        totalCategories: count.length,
       });
     } catch (error) {
       res.status(500).json({ error: "Error al obtener las categorias" });
     }
   },
-  // uniqueValues: async (req, res) => {
-  //   let { page = 1, limit = 5 } = req.query;
-  //   page = parseInt(page);
-  //   limit = parseInt(limit);
-  //   try {
-  //     const uniqueValues = await db.Categoria.findAll({
-  //       attributes: [
-  //         [Sequelize.fn("DISTINCT", Sequelize.col("categoria")), "categoria"],
-  //       ],
-  //     });
-  //     // console.log(uniqueValues);
 
-  //     // Mapea los resultados para devolver solo los valores únicos en formato JSON
-  //     const uniqueValuesArray = uniqueValues.map((item) => item);
-  //     console.log(uniqueValuesArray);
-
-  //     res.json({
-  //       uniqueValuesArray,
-  //       currentPage: page,
-  //       totalPages,
-  //       totalCategories: count,
-  //     });
-  //   } catch (error) {
-  //     console.error("Error en la consulta:", error); // Loguea el error en la consola del servidor
-  //     res.status(500).json({ error: error.message || "Error en el servidor" });
-  //   }
-  // },
 };
 module.exports = categoriasController;
--- back/database/config/config.js
+++ back/database/config/config.js
@@ -9,14 +9,14 @@ module.exports = {
   test: {
     username: "root",
     password: "",
-    database: "gooolstore",
+    database: "golazo",
     host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
     username: "root",
     password: "",
-    database: "gooolstore",
+    database: "golazo",
     host: "127.0.0.1",
     dialect: "mysql",
   },
